62c798736e8e58fface5f3e245a4956d9a528616
zannatulferdous0162 <zannatulferdous0162@gmail.com>
2024-09-02 19:37:14 +0600

    add card function

--- app.js
+++ app.js
@@ -2,14 +2,39 @@
 const allBtn = document.getElementsByClassName('add-btn');
 // console.log(allBtn);
 
-for(const btn of allBtn){
-    btn.addEventListener('click',function(event){
+for (const btn of allBtn) {
+    btn.addEventListener('click', function (event) {
         const name = event.target.parentNode.childNodes[1].innerText;
-        const price =  event.target.parentNode.childNodes[3].childNodes[1].innerText;
+        const price = event.target.parentNode.childNodes[3].childNodes[1].innerText;
         const category = event.target.parentNode.childNodes[5].childNodes[1].innerText;
-        console.log(category);
-        
+
+
+        const selectedContainer = document.getElementById('selected-players-container');
+
+        const div = document.createElement('div');
+        div.classList.add('selected-players');
+        const p1 = document.createElement('p');
+        const p2 = document.createElement('p');
+        const p3 = document.createElement('p');
+
+        p1.innerText = name;
+        p2.innerText = price;
+        p3.innerText = category;
+
+        div.appendChild(p1);
+        div.appendChild(p2);
+        div.appendChild(p3);
+
+        selectedContainer.appendChild(div);
+
+        updateTotalCost(price);
     })
+}
+
+function updateTotalCost(value){
+    const totalCost =   getConvartedVlue('total-cost');
+    const sum = totalCost + parseInt(value);
+    document.getElementById('total-cost').innerText = sum;
 }
 
 
@@ -18,7 +43,7 @@ for(const btn of allBtn){
 // const cartCount= getConvartedVlue('cart');
 // const leftCount = getConvartedVlue('left');
 
-function getConvartedVlue(id){
+function getConvartedVlue(id) {
     const price = document.getElementById(id).innerText;
     const convertPrice = parseInt(price);
     return convertPrice;
